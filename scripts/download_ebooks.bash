19aca996632016ac3ed39abd148bbbbd94a0826b
JacobPuff <jacob@todandlorna.com>
2021-03-19 16:22:44 -0600

    Use until loop to get an exact number of files

--- scripts/download_ebooks.bash
+++ scripts/download_ebooks.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
-for i in {0..155}
+declare -i count=0
+declare -i i=0
+until [[ count -eq 100 ]]
 do
     text=`curl -f https://www.gutenberg.org/files/${i}/${i}-0.txt`
     if [[ $? -eq 0 ]]
     then
+        count=count+1
         echo $text > "${i}.txt"
     fi
+    i=i+1
 done
