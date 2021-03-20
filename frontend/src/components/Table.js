60443b91810f83fbac87ad361e51fb4e610f84a7
JacobPuff <jacob@todandlorna.com>
2021-03-19 22:03:27 -0600

    Add Table file.

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,16 @@
+function FormatTextFiles() {
+
+}
+
+function Table() {
+    return (
+        <table class="table">
+            <thead>
+                <th scope="col">File Num</th>
+                <th scope="col">"Report" Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">{/*Text*/}</th>
+            </thead>
+        </table>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
+// import Table from 'components/Table';
+
 function App() {
     return (
         <div class="app border border-primary">
             <h4 class="title border-bottom border-3 border-primary">Hello World</h4>
+            {/* <Table/> */}
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('root')
 )
