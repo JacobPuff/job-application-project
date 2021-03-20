3fa17e8465c651826c9354ec31f773f590e9edd5
JacobPuff <jacob@todandlorna.com>
2021-03-20 12:14:25 -0600

    Fix merge conflict

--- backend/src/structs/structs.go
+++ backend/src/structs/structs.go
@@ -8,11 +8,7 @@ type FileMetaData struct {
 	StartingText string `json:"startingText"`
 }
 
-<<<<<<< HEAD
 type FileTagData struct {
-=======
-type FileTagData {
->>>>>>> 7d9d66a426c4e5f13f57337f38e75215703daae3
 	FileNum int `json:"fileNum"`
 	Tags []string `json:"tags"`
 }
