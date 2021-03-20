7d9d66a426c4e5f13f57337f38e75215703daae3
JacobPuff <jacob@todandlorna.com>
2021-03-20 11:19:08 -0600

    Add FileTagData struct

--- backend/src/structs/structs.go
+++ backend/src/structs/structs.go
@@ -1,9 +1,14 @@
 package structs
 
 type FileMetaData struct {
-	FileNum int `json:"fileNum`
+	FileNum int `json:"fileNum"`
 	Title string `json:"title"`
 	Subtitle string `json:"subtitle"`
 	Author string `json:"author"`
 	StartingText string `json:"startingText"`
+}
+
+type FileTagData {
+	FileNum int `json:"fileNum"`
+	Tags []string `json:"tags"`
 }
