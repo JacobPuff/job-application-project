c5150e6e4c8b9c54343bc0378de5101fe7b695bd
JacobPuff <jacob@todandlorna.com>
2021-03-20 11:19:08 -0600

    Add FileTagData struct

---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["css-loader"]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    }
+};
